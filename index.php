e936ba2a43c157c2e60ee6cae9e939c875508f05
cleverchao <cleverchao@163.com>
2014-03-18 22:05:41 +0800

    slider

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" href="css/slideme.css" type="text/css" media="all"/>
 <link rel="stylesheet" href="css/main.css" type="text/css" media="all"/>
-<title>A kaopu man</title>
+<title>Some靠谱男青年</title>
 </head>
 <body>
 
@@ -14,9 +14,9 @@
 	<div class="c-header">
 		<div id="slider">
 		    <ul class="slideme">
-		        <li id="knowledge"><img src="image/1.jpg"></li>
-		        <li id="music"><img src="image/2.jpg"></li>
-		        <li id="daddy"><img src="image/3.jpg"></li>
+		        <li id="knowledge"><img class="slide-image" src="image/1.jpg"></li>
+		        <li id="music"><img class="slide-image" src="image/2.jpg"></li>
+		        <li id="daddy"><img class="slide-image" src="image/3.jpg"></li>
 		    </ul>
 		</div>
 	</div>
@@ -27,7 +27,7 @@
 <div class="footer">
 </div>
 
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="js/jquery.slideme-1.21.71.js"></script>
 <script type="text/javascript" src="js/main.js"></script>
 
