a7168053815aee66323965a204dde234986994f4
zhengjiachao <cleverchao@163.com>
2014-03-18 17:49:30 +0800

    slider1

--- index.php
+++ index.php
@@ -4,18 +4,28 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" href="css/slideme.css" type="text/css" media="all"/>
 <link rel="stylesheet" href="css/main.css" type="text/css" media="all"/>
-<title>welcome</title>
+<title>A kaopu man</title>
 </head>
 <body>
 
-<div id="foo">
-    <ul class="slideme"> <!-- Class "slideme" is mandatatory! -->
-        <li id="knowledge"></li>
-        <li id="music"></li>
-        <li id="daddy"></li>
-    </ul>
+<div class="header"></div>
+
+<div class="container">
+	<div class="c-header">
+		<div id="slider">
+		    <ul class="slideme">
+		        <li id="knowledge"><img src="image/1.jpg"></li>
+		        <li id="music"><img src="image/2.jpg"></li>
+		        <li id="daddy"><img src="image/3.jpg"></li>
+		    </ul>
+		</div>
+	</div>
+	<div class="content">
+	</div>
 </div>
 
+<div class="footer">
+</div>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script type="text/javascript" src="js/jquery.slideme-1.21.71.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-	$('#foo').slideme({
-		arrows: true,
-		loop: true
+	$('#slider').slideme({
+		loop: true,
+		interval: 1500,
+		transition: "fade",
+		pagination: "thumbs",
+		autoslide: true
 	});
 });
